b116490d698aeff8d377043f235031f58c54bb52
Luciana <lucianaqueiroz098@hotmail.com>
2021-01-28 13:31:55 -0300

    atualizacao

--- src/atualizar/atualizar-tarefa.js
+++ src/atualizar/atualizar-tarefa.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {Button, Form, Jumbotron, Modal} from 'react-bootstrap';
 import {navigate, A} from 'hookrouter';
 
 
 function AtualizarTarefa(props) {
+
+    const [exibirModal, setExibirModal] = useState(false);
+    const [formValidado, setFormValidado] = useState(false);
+    const [tarefa, setTarefa] = useState('');
+    const [carregarTarefa, setCarregarTarefa] = useState(true);
+
+
+    useEffect(()=>{
+
+        //p limitar uma unica vez, vamos colocar o if
+
+        if(carregarTarefa){
+            const tarefasDb = localStorage['tarefas'];
+            const tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            const tarefa = tarefas.filter(
+                t => t.id === parseInt(props.id)  
+            )[0];
+            setTarefa(tarefa.nome);
+
+            setCarregarTarefa(false);
+        }
+
+    }, [carregarTarefa, props]);
 
 
 
@@ -13,16 +36,45 @@ function AtualizarTarefa(props) {
         navigate('/');
     }
 
+    function handleFecharModal(){
+        navigate('/');
+    }
+
+    function atualizar(event){
+        event.preventDefault();
+        setFormValidado(true);
+        if(event.currentTarget.checkValidity() === true){
+            //obter as tarefas
+            const tarefasDb = localStorage['tarefas'];
+            let tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            //persistir a tarefa atualziada
+            tarefas = tarefas.map(tarefaObj => {
+                if(tarefaObj.id === parseInt(props.id)){
+                    tarefaObj.nome = tarefa;
+                }
+                return tarefaObj;
+            });
+            localStorage['tarefas'] = JSON.stringify(tarefas);
+            setExibirModal(true);
+        }
+
+    }
+
+    function handleTxtTarefa(event){
+        setTarefa(event.target.value);
+        //o valor do txt
+    }
+
     return (
     
         <div>
             <h3 className="text-center">Atualizar</h3>
             <Jumbotron>
-                <Form noValidate>
+                <Form onSubmit={atualizar} noValidate validated={formValidado}>
                     <Form.Group>
                         <Form.Label>Tarefa</Form.Label>
                         <Form.Control type="text" placeholder="Digite a tarefa"
-                            minLength="5" maxLength="100" required data-testid="txt-tarefa">
+                            minLength="5" maxLength="100" required data-testid="txt-tarefa" value={tarefa} onChange={handleTxtTarefa}>
                         </Form.Control>
                         <Form.Control.Feedback type="invalid">
                             A tarefa deve conter ao menos 5 caracteres
@@ -38,6 +90,22 @@ function AtualizarTarefa(props) {
                         </A>
                     </Form.Group>
                 </Form>
+
+                <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+                    <Modal.Header closeButton>
+                        <Modal.Title>Sucesso </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Tarefa atualizado com sucesso!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" onClick={handleFecharModal}>
+                            Continuar
+                        </Button>
+
+                    </Modal.Footer>
+                </Modal>
+
             </Jumbotron>
         </div>
     );
--- src/atualizar/atualizar-tarefa.test.js
+++ src/atualizar/atualizar-tarefa.test.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AtualizarTarefa from './atualizar-tarefa';
-//import Tarefa from '../models/tarefa.model';
+import Tarefa from '../models/tarefa.model';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Teste do componente de atualiazação de tarefas', () => {
 
 	const tarefaId = 1;
-	/*const tarefa = new Tarefa(tarefaId, 'Nova tarefa', false);
+	const tarefa = new Tarefa(tarefaId, 'Nova tarefa', false);
 
 	beforeEach(() => {
 		localStorage['tarefas'] = JSON.stringify([tarefa]);
 	});
-*/
+
 	it('deve renderizar o componente sem erros', () => {
 	  const div = document.createElement('div');
 	  ReactDOM.render(<AtualizarTarefa id={tarefaId} />, div);
 	  ReactDOM.unmountComponentAtNode(div);
 	});
-/*
+
 	it('deve exibir a modal de sucesso ao atualizar uma tarefa', () => {
 		const { getByTestId } = render(<AtualizarTarefa id={tarefaId} />);
 		fireEvent.click(getByTestId('btn-atualizar'));
@@ -34,5 +34,5 @@ describe('Teste do componente de atualiazação de tarefas', () => {
 		const tarefasDb = JSON.parse(localStorage['tarefas']);
 		expect(tarefasDb[0].nome).toBe(nomeTarefaAtualizada);
 	});
-*/
+
 });
